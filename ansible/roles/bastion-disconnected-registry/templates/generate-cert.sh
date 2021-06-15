767a7878482f7478f5d5a8f41a4ed0eb19b05e5b
Alex Krzos <akrzos@redhat.com>
2021-06-15 10:45:09 -0400

    Add ipv6 clusters

---
+++ ansible/roles/bastion-disconnected-registry/templates/generate-cert.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+host_fqdn=$( hostname --long )
+cert_c="US"               # Country Name (C, 2 letter code)
+cert_s="North Carolina"   # Certificate State (S)
+cert_l="Raleigh"          # Certificate Locality (L)
+cert_o="RedHat"           # Certificate Organization (O)
+cert_ou=""                # Certificate Organizational Unit (OU)
+cert_cn="${host_fqdn}"    # Certificate Common Name (CN)
+
+openssl req \
+    -newkey rsa:4096 \
+    -nodes \
+    -sha256 \
+    -keyout {{ registry_path }}/certs/domain.key \
+    -x509 \
+    -days 365 \
+    -out {{ registry_path }}/certs/domain.crt \
+    -addext "subjectAltName = DNS:${host_fqdn}" \
+    -subj "/C=${cert_c}/ST=${cert_s}/L=${cert_l}/O=${cert_o}/OU=${cert_ou}/CN=${cert_cn}"
