b095d3b1e493b95efe46dea8de3674a416a81de0
Alex Krzos <akrzos@redhat.com>
2021-09-13 17:19:04 -0400

    Add testcase 5 and improve test case scripts

--- tests/nodedensity/run_all.sh
+++ tests/nodedensity/run_all.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 version=$(oc version -o json | jq -r '.openshiftVersion')
-network="sdn"
-#network="ovn"
+
+sdn_pods=$(oc get po -n openshift-sdn --no-headers | wc -l)
+network="ovn"
+if [[ $sdn_pods -gt 0 ]]; then
+  network="sdn"
+fi
 
 time ./testcase-1.sh ${version}-${network} | tee ${version}-${network}-tc1.log
 sleep 300
@@ -11,3 +15,5 @@ sleep 300
 time ./testcase-3.sh ${version}-${network} | tee ${version}-${network}-tc3.log
 sleep 300
 time ./testcase-4.sh ${version}-${network} | tee ${version}-${network}-tc4.log
+sleep 300
+time ./testcase-5.sh ${version}-${network} | tee ${version}-${network}-tc5.log
