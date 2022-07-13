70cc53f4b5c399acca9590f1cd0f78f39a695573
Noreen21 <nchhabra@redhat.com>
2022-07-13 14:27:25 -0400

    add install jmespath to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,5 @@ python3 -m venv .ansible
 source .ansible/bin/activate
 pip3 install --upgrade pip
 pip3 install ansible netaddr
+pip3 install jmespath
 ansible-galaxy collection install ansible.utils
