79f111762cdbd3483f80cee334c26e3594377fb5
Raul Sevilla <rsevilla@redhat.com>
2024-12-05 10:31:25 +0100

    Quieter bootstrap
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 python3 -m venv .ansible
 source .ansible/bin/activate
-pip3 install --upgrade pip
-pip3 install ansible netaddr
-pip3 install jmespath --force
+pip3 install -q --upgrade pip
+pip3 install -q ansible netaddr
+pip3 install -q jmespath --force
 ansible-galaxy collection install ansible.utils --force
