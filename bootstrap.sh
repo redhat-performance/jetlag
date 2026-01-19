ac7c3e9d8f37579a2db40b3c767a9cb42d83497c
Marius Cornea <mcornea@redhat.com>
2026-01-19 12:58:26 +0200

    Retrieve quads api server from ansible/vars/lab.yml

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,5 @@ source .ansible/bin/activate
 pip3 install -q --upgrade pip
 pip3 install -q 'ansible<12.0.0' netaddr
 pip3 install -q jmespath --force
+pip3 install -q yq
 ansible-galaxy collection install ansible.utils --force
