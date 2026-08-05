ab78db6e537539c7af9acf629a4a6e60fa24c789
Raviteja Sahukari <rsahukar@redhat.com>
2026-08-05 12:55:01 +0530

    fix: pin argcomplete to 3.7.0 to resolve Python 3.9 TypeError in ansible-galaxy
    Signed-off-by: Raviteja Sahukari <rsahukar@redhat.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,8 @@
 python3 -m venv .ansible
 source .ansible/bin/activate
 pip3 install -q --upgrade pip
+# pin argcomplete to 3.7.0 in bootstrap script due to TypeError
+pip3 install -q argcomplete==3.7.0
 pip3 install -q 'ansible<12.0.0' netaddr
 pip3 install -q jmespath --force
 pip3 install -q yq
